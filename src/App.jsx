2638f025851e0a58ca416f93c9c58f20b1a2393b
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-11-06 16:23:44 +0900

    feat: create Mainpage
    App.jsx 일부 수정
    main 페이지 promotion carousel 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import MainPage from './pages/MainPage/main';
 
 const App = () => {
   return (
     <>
-      <Header path='/' component={Header} />
+      <Header />
       <Switch>
+		  <Route path='/' exact component={MainPage} />
       </Switch>
       <Footer />
     </>
---
+++ src/pages/MainPage/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import './main.scss';
+
+function main() {
+	var settings ={
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll:1
+	};
+	return (
+		<div className="main-container">
+			<div className="promotion-container">
+				<Slider {...settings}>
+					<div>
+						<img src="https://wiseux.speedgabia.com/imdak/contents/main_banner_promotion/20210831_1630394465.jpg" />
+					</div>
+					<div>
+						<img src="https://wiseux.speedgabia.com/imdak/contents/main_banner_promotion/20210205_1612495481.jpg" />
+					</div>
+				</Slider>
+			</div>
+			<div className="prefer-container">
+				인기상품
+			</div>
+		</div>
+	)
+}
+
+export default main;
